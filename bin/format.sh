9d36a0d70a32f94c8b8cf5948807be7a1d2dd5da
bryce-s <bryces@umich.edu>
2018-04-15 11:45:24 -0400

    more tests

--- bin/format.sh
+++ bin/format.sh
@@ -2,3 +2,4 @@ set -e
 set -x
 autopep8 --in-place --aggressive --aggressive csvCardParse.py
 autopep8 --in-place --aggressive --aggressive classes/*.py
+autopep8 --in-place --aggressive --aggressive unittests/*.py
--- unittests/CSV_WriterTests.py
+++ unittests/CSV_WriterTests.py
@@ -2,6 +2,8 @@ import unittest
 import sys
 from classes.CSV_Writer import CSV_Writer
 from classes.ImageProcessor import ImageProcessor
+import os
+import shutil
 
 class TestCSV_Writer(unittest.TestCase):
     
@@ -16,6 +18,16 @@ class TestCSV_Writer(unittest.TestCase):
     def test_generateString(self):
         """checks if string is generated"""
         assert(self.writer.generateRandomImageString())
+
+    def test_combineHTML_Tag(self):
+        assert(self.writer.combineToHTML_Tag("hi") == "<img src='hi'/>")
+
+    def test_writeCSV(self):
+        testOutputName = str("testOut")
+        self.writer.writeCSV(testOutputName, self.basicTestDir)
+        assert(os.path.isdir(testOutputName))
+        shutil.rmtree(testOutputName)
+
 
 if __name__ == '__main__':
     unittest.main()
